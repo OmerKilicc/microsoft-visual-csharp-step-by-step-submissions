48e75c1851731b7242c6f09565ae9646ce21f3ef
Omer Faruk Kilic <omeerfkiliic@gmail.com>
2024-02-11 19:05:09 +0300

    ch17

--- Chapter 17/BuildTree - Complete/BuildTree/obj/Debug/net6.0/BuildTree.AssemblyInfo.cs
+++ Chapter 17/BuildTree - Complete/BuildTree/obj/Debug/net6.0/BuildTree.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f06242e4120611c5ea7046cb8142d79932075e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f53cff7d63d42e2f0c3452a4ebe2aa65d4a660")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildTree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
